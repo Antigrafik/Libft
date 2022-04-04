4423a9e016644140b1920020f46ea008a4a6daca
Maialen Murgia olalla <mmurgia-@c3r1s1.42urduliz.com>
2022-04-04 13:05:49 +0200

    Ejercicios corregidos

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mmurgia- <mmurgia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 12:21:38 by mmurgia-          #+#    #+#             */
-/*   Updated: 2022/03/31 13:36:16 by mmurgia-         ###   ########.fr       */
+/*   Updated: 2022/04/04 10:12:51 by mmurgia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	*ft_memset(void *str, int c, size_t n)
 	while (i < n)
 	{
 		dest[i] = c;
+		i++;
 	}
 	return (str);
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,20 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmurgia- <mmurgia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/28 12:21:38 by mmurgia-          #+#    #+#             */
-/*   Updated: 2022/04/04 10:07:57 by mmurgia-         ###   ########.fr       */
+/*   Created: 2022/04/04 10:28:24 by mmurgia-          #+#    #+#             */
+/*   Updated: 2022/04/04 12:40:44 by mmurgia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isprint(int c)
+#include <string.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	if (c >= 32 && c < 127)
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (src == NULL || dst == NULL)
 	{
-		return (1);
+		return (0);
 	}
-	return (0);
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	if (dstsize != 0)
+	{
+		while (j < (dstsize - 1) && src[j] != '\0')
+		{
+			dst[j] = src[j];
+			j++;
+		}
+		dst[j] = '\0';
+	}
+	return (i);
 }
