f407fb039eed0a8ac36606bdcc2b89af367561f8
Antigrafik <102741843+Antigrafik@users.noreply.github.com>
2022-04-14 12:48:41 +0200

    Ejercicios FD

---
+++ ft_putendl_fd.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmurgia- <mmurgia-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 12:30:06 by mmurgia-          #+#    #+#             */
+/*   Updated: 2022/04/14 12:38:24 by mmurgia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	unsigned int	len;
+	unsigned int	i;
+
+	i = 0;
+	len = ft_strlen(s);
+	while (i < len)
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+
+int	main(void)
+{
+	char	*c;
+	int		fd;
+
+	c = "MAI";
+	fd = 1;
+	ft_putendl_fd(c, fd);
+}
---
+++ ft_putstr_fd.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmurgia- <mmurgia-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 11:27:29 by mmurgia-          #+#    #+#             */
+/*   Updated: 2022/04/14 12:27:32 by mmurgia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	unsigned int	len;
+	unsigned int	i;
+
+	i = 0;
+	len = ft_strlen(s);
+	while (i < len)
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+/* int	main(void)
+{
+	char	*c;
+	int		fd;
+
+	c = "MAI";
+	fd = 1;
+	ft_putstr_fd(c, fd);
+} */
